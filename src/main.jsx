6ee7417b1a4af501a259f0b9ce6948ccc9fbd48c
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-05 15:03:42 +0400

    style: complete Signin component's style

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="726769127592-jrtbuup8lqq9e30nvdcr03djlk83grna.apps.googleusercontent.com">
       <App />
+    </GoogleOAuthProvider>;
   </StrictMode>,
 );
