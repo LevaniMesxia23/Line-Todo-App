185bec7f56e8ab5b8b00a9801b1bdd512a039f18
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-05 19:44:12 +0400

    style: add TaskSection  component's ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.jsx"
+import TasksSection from "../components/TasksSection.jsx"
 
 function Home() {
   return (
     <div>
       <Header />
+      <TasksSection />
     </div>
   )
 }
